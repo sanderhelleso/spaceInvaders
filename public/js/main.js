e03c75146f68801208745f8535fca6f922c2bf2d
Sanderhelleso <sanderhelleso@hotmail.com>
2018-11-11 17:48:39 -0800

    added moving bg

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,24 @@
 window.onload = startApp;
 
+
 function startApp() {
-    console.log(123);
+    gameLoop();
+}
+
+function gameLoop() {
+    setInterval(() => {
+        moveBg();
+    }, 100);
+}
+
+let bgPosY = 0;
+function moveBg() {
+    const bg = document.querySelector('#game-window');
+    bg.style.backgroundPosition = `100% ${bgPosY}%`;
+
+    if (bgPosY >= 100) {
+        bgPosY = 0;
+    }
+
+    bgPosY += 5;
 }

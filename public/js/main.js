b6670c9cb815ba1821e5995becd23d0d840e7b09
Sanderhelleso <sanderhelleso@hotmail.com>
2018-11-11 19:56:10 -0800

    added player movement

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ window.onload = startApp;
 
 
 function startApp() {
+    initPlayer();
     gameLoop();
 }
 
@@ -11,6 +12,8 @@ function gameLoop() {
     }, 100);
 }
 
+
+// animate game background
 let bgPosY = 0;
 function moveBg() {
     const bg = document.querySelector('#game-window');
@@ -20,5 +23,47 @@ function moveBg() {
         bgPosY = 0;
     }
 
-    bgPosY += 5;
+    bgPosY += 10;
+}
+
+let player;
+let posX = 50;
+function initPlayer() {
+    player = document.querySelector('#player-cont');
+    player.style.left = `${posX}%`;
+    player.style.transform = `translate(${-posX}%)`;
+
+    document.body.addEventListener('keydown', (e) => {
+
+        // left
+        if (e.keyCode === 65 || e.keyCode === 37) {
+            if (posX >= 2) {
+                moveLeft();
+            }
+        }
+
+        // right
+        else if (e.keyCode === 68 || e.keyCode === 39) {
+            if (posX <= 86) {
+                moveRight();
+            }
+        }
+
+    });
+
+    document.body.addEventListener('keyup', () => {
+        player.style.transform = 'rotate(0deg)';
+    });
+}
+
+function moveLeft() {
+    posX -= 1.5;
+    player.style.transform = 'rotate(-10deg)';
+    player.style.left = `${posX}%`;
+}
+
+function moveRight() {
+    posX += 1.5;
+    player.style.transform = 'rotate(10deg)';
+    player.style.left = `${posX}%`;
 }
